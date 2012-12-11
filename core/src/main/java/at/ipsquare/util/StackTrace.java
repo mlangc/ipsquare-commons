0c13a04e9a6863121834904907947dda450d3cfd
Matthias Langer <mlangc@gmx.at>
2012-12-11 16:28:22 +0100

    Working on StackTrace related APIs.

---
+++ core/src/main/java/at/ipsquare/util/StackTrace.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2012 IP SQUARE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.ipsquare.util;
+
+import java.util.Arrays;
+
+/**
+ * Utility methods for dealing with stack traces.
+ * 
+ * @author Matthias Langer
+ */
+public final class StackTrace
+{
+    /**
+     * Returns the current stack trace, starting with the invocation of this method (unlike {@link Thread#getStackTrace()}).
+     * 
+     * <h4>Note:</h4>
+     *  In theory this method might return an empty array if the VM has no stack trace information
+     *  concerning the current thread. With a decent VM however, this should never happen.
+     */
+    public static StackTraceElement[] get()
+    {
+        StackTraceElement[] elems =  Thread.currentThread().getStackTrace();
+        
+        boolean seenThisClass = false;
+        int i = 0;
+        for(; i < elems.length; ++i)
+        {
+            StackTraceElement elem = elems[i];
+            try
+            {
+                Class<?> clazz = Class.forName(elem.getClassName());
+                if(StackTrace.class.equals(clazz))
+                    seenThisClass = true;
+                else if(seenThisClass)
+                    break;
+            }
+            catch(ClassNotFoundException e)
+            {
+                throw new RuntimeException("Could not find class from stack trace; trouble awaits!", e);
+            }
+        }
+        
+        return Arrays.copyOfRange(elems, i, elems.length);
+    }
+    
+    private StackTrace()
+    {
+        
+    }
+}
---
+++ core/src/test/java/at/ipsquare/util/TestStackTrace.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2012 IP SQUARE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.ipsquare.util;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link StackTrace}.
+ * 
+ * @author Matthias Langer
+ */
+public class TestStackTrace
+{
+    /**
+     * Tests {@link StackTrace#get()}.
+     */
+    @Test
+    public void testGet() throws ClassNotFoundException
+    {
+        StackTraceElement[] stackTrace = StackTrace.get();
+        assertThat(stackTrace.length, not(0));
+        
+        StackTraceElement first = stackTrace[0];
+        assertEquals(TestStackTrace.class, Class.forName(first.getClassName()));
+        assertThat(first.getMethodName(), equalTo("testGet"));
+    }
+}
